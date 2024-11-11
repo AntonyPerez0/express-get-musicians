5de1a2acd6905da714e443275607b51cf3ca4e28
Antony Perez <antonyperez0@yahoo.com>
2024-11-11 09:21:10 -0800

    done

--- index.test.js
+++ index.test.js
@@ -1,22 +1,23 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("GET /musicians should return all musicians", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+    const responseData = response.body;
+    expect(Array.isArray(responseData)).toBe(true);
+    expect(responseData.length).toBeGreaterThan(0);
+    expect(responseData[0]).toHaveProperty("name");
+    expect(responseData[0]).toHaveProperty("instrument");
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
-
-
-
-
-
-
+//TODO: Create a GET /musicians route to return all musicians
+app.get("/musicians", async (req, res) => {
+  try {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = app;
